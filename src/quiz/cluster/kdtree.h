6558a68edada6873cd579e7accbce8bd1a5a41da
uchaudh <uchaudh@g.clemson.edu>
2022-06-09 23:22:09 -0400

    Recursive function for kd tree

--- src/quiz/cluster/kdtree.h
+++ src/quiz/cluster/kdtree.h
@@ -36,10 +36,28 @@ struct KdTree
 		delete root;
 	}
 
+	void insertHelper(Node** node, int depth, std::vector<float> point, int id)
+	{
+		if(*node == NULL)
+		{
+			*node = new Node(point, id);
+		}
+		else
+		{
+			int current_depth = depth % 2;
+
+			if((point[current_depth]) < ((*node)->point[current_depth]))
+				insertHelper(&((*node)->left),depth+1, point, id);
+			else
+				insertHelper(&((*node)->right),depth+1, point, id);
+		}
+	}
+
 	void insert(std::vector<float> point, int id)
 	{
 		// TODO: Fill in this function to insert a new point into the tree
-		// the function should create a new node and place correctly with in the root 
+		// the function should create a new node and place correctly with in the root
+		insertHelper(&root,0,point,id);
 
 	}
 
